9cad7170ae47dd143747d0a2c38905a0e8d7ab0c
lauriavictor7 <victor7lauria@live.com>
2018-10-29 20:54:31 -0300

    Presentation page removed.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -26,7 +26,6 @@ module.exports = {
                 algolia: {},
                 nav: [
                     { text: 'Home', link: '/' },
-                    { text: 'Introduciton', link: '/introduction/' },
                     { text: 'Administrator Guide', link: '/administration/' },
                     { text: 'Developer Guide', link: '/developer/' },
                     { text: 'ManGve', link: 'http://mangve.org' },
@@ -34,7 +33,6 @@ module.exports = {
                 ],
                 sidebar: [
                     '/',
-                    '/introduction/',
                     '/administration/',
                     '/developer/'
                 ],
@@ -51,7 +49,6 @@ module.exports = {
                 },
                 nav: [
                     { text: 'Início', link: '/pt-br/' },
-                    { text: 'Introdução', link: '/pt-br/introduction/' },
                     { text: 'Guia do Administrador', link: '/pt-br/administration/' },
                     { text: 'Guia do Desenvolvedor', link: '/pt-br/developer/' },
                     { text: 'ManGve', link: 'http://mangve.org' },
@@ -60,7 +57,6 @@ module.exports = {
                 algolia: {},
                 sidebar: [
                     '/pt-br/',
-                    '/pt-br/introduction/',
                     '/pt-br/administration/',
                     '/pt-br/developer/'
                 ]
